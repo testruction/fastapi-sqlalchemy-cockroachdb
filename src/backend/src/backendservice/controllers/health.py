183e3cebbe47b5175902dba3826b08eb055d6a2d
Florian JUDITH <florian.judith.b@gmail.com>
2023-07-20 22:25:46 -0400

    Update health.py

--- src/backend/src/backendservice/controllers/health.py
+++ src/backend/src/backendservice/controllers/health.py
@@ -5,6 +5,6 @@ from fastapi import APIRouter
 class HealthApis():
     router = APIRouter()
 
-    @router.get('/v1/health', status_code=200)
+    @router.get('/health', status_code=200)
     def health_page():
         return "OK"
