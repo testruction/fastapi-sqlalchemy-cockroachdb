abf3ffbc8bb0da1da07ea2f163d685d9a01f61c3
Florian JUDITH <florian.judith.b@gmail.com>
2023-08-09 16:10:47 -0400

    Fixed arguments conflicts

--- src/backend/src/backendservice/config.py
+++ src/backend/src/backendservice/config.py
@@ -14,19 +14,19 @@ parser.add_argument('--debug',
 parser.add_argument('--trace-stdout',
                     help="Show OpenTelemetry output to console",
                     action="store_true")
-parser.add_argument('--host',
+parser.add_argument('--database-host',
                     type=str,
                     help='Hostname, fully qualified name or IP address',
                     default=os.environ.get('COCKROACH_HOST', default='localhost'))
-parser.add_argument('--port',
+parser.add_argument('--database-port',
                     type=int,
                     help='Listening port',
                     default=os.environ.get('COCKROACH_PORT', default=26257))
-parser.add_argument('--username',
+parser.add_argument('--database-username',
                     type=str,
                     help='Database user login name',
                     default=os.environ.get('COCKROACH_USER', default='root'))
-parser.add_argument('--password',
+parser.add_argument('--database-password',
                     type=str,
                     help='Database user login password',
                     default=os.environ.get('COCKROACH_PASSWORD', default='dummypassword'))
@@ -48,10 +48,10 @@ class Config(object):
     SITE_NAME = 'app'
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = URL.create(drivername='cockroachdb',
-                                         username=args.username,
-                                         password=args.password,
-                                         host=args.host,
-                                         port=args.port,
+                                         username=args.database_username,
+                                         password=args.database_password,
+                                         host=args.database_host,
+                                         port=args.database_port,
                                          database=args.database)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
