18206a139d5603d98a687f8ca76b416aabf2327c
Helm Student <student.helm@testruction.lab>
2023-08-29 16:16:15 -0400

    Simplyfied code

--- src/backend/src/backendservice/models/postgres/crud.py
+++ src/backend/src/backendservice/models/postgres/crud.py
@@ -18,9 +18,8 @@ def create(db: Session, identity: schemas.FakenamesCreate):
     """ Creates a database entry """
     response = None
 
-    with tracer.start_as_current_span(name='create'):
-        current_span = trace.get_current_span()
-        current_span.set_attributes({'enduser.id': get_openid_user(Request)})
+    with tracer.start_as_current_span(name='create') as span:
+        span.set_attributes({'enduser.id': get_openid_user(Request)})
 
         try:
             response = models.Fakenames(**identity.dict())
@@ -35,10 +34,10 @@ def create(db: Session, identity: schemas.FakenamesCreate):
             e = traceback.format_exc()
             logger.error(f'Creating row failed!\n{e}')
             status = trace.status.Status(trace.StatusCode.ERROR)
-            current_span.record_exception(e)
+            span.record_exception(e)
             response = False
 
-        current_span.set_status(status)
+        span.set_status(status)
     return response
 
 
@@ -46,9 +45,8 @@ def read_all(db: Session, skip: int = 0, limit: int = 100):
     """ Retreive all records from the table """
     response = None
 
-    with tracer.start_as_current_span(name='read_all'):
-        current_span = trace.get_current_span()
-        current_span.set_attributes({'enduser.id': get_openid_user(Request)})
+    with tracer.start_as_current_span(name='read_all') as span:
+        span.set_attributes({'enduser.id': get_openid_user(Request)})
         try:
             response = db.query(models.Fakenames).offset(skip).limit(limit).all()
 
@@ -57,9 +55,9 @@ def read_all(db: Session, skip: int = 0, limit: int = 100):
             e = traceback.format_exc()
             logger.error(f'Reading rows failed!\n{e}')
             status = trace.status.Status(trace.StatusCode.ERROR)
-            current_span.record_exception(e)
+            span.record_exception(e)
 
-        current_span.set_status(status)
+        span.set_status(status)
     return response
 
 
@@ -67,10 +65,9 @@ def read(db: Session, guid: str):
     """ Retrieves a given identity by its GUID """
     response = None
 
-    with tracer.start_as_current_span(name='read'):
-        current_span = trace.get_current_span()
-        current_span.set_attributes({'enduser.id': get_openid_user(Request)})
-        current_span.set_attributes({'fakenames.guid': guid})
+    with tracer.start_as_current_span(name='read') as span:
+        span.set_attributes({'enduser.id': get_openid_user(Request)})
+        span.set_attributes({'fakenames.guid': guid})
 
         try:
             response = db.query(models.Fakenames).filter(models.Fakenames.guid == guid).first()
@@ -80,9 +77,9 @@ def read(db: Session, guid: str):
             e = traceback.format_exc()
             logger.error(f'Reading row "{guid}" failed!\n{e}')
             status = trace.status.Status(trace.StatusCode.ERROR)
-            current_span.record_exception(e)
+            span.record_exception(e)
 
-        current_span.set_status(status)
+        span.set_status(status)
     return response
 
 
@@ -91,9 +88,8 @@ def update(db: Session, identity: schemas.FakenamesCreate) -> bool:
     response = None
     guid = None
 
-    with tracer.start_as_current_span(name='update'):
-        current_span = trace.get_current_span()
-        current_span.set_attributes({'enduser.id': get_openid_user(Request)})
+    with tracer.start_as_current_span(name='update') as span:
+        span.set_attributes({'enduser.id': get_openid_user(Request)})
 
         try:
             response = models.Fakenames(**identity.dict())
@@ -106,10 +102,10 @@ def update(db: Session, identity: schemas.FakenamesCreate) -> bool:
             e = traceback.format_exc()
             logger.error(f'Updating row "{guid}" failed!\n{e}')
             status = trace.status.Status(trace.StatusCode.ERROR)
-            current_span.record_exception(e)
+            span.record_exception(e)
             response = False
 
-        current_span.set_status(status)
+        span.set_status(status)
     return response
 
 
@@ -124,9 +120,8 @@ def delete(db: Session, guid: str) -> bool:
                 'surname': None,
                 'guid': guid}
 
-    with tracer.start_as_current_span(name='delete'):
-        current_span = trace.get_current_span()
-        current_span.set_attributes({'enduser.id': get_openid_user(Request)})
+    with tracer.start_as_current_span(name='delete') as span:
+        span.set_attributes({'enduser.id': get_openid_user(Request)})
 
         try:
             r = db.query(models.Fakenames).filter(models.Fakenames.guid == guid).first()
@@ -147,7 +142,7 @@ def delete(db: Session, guid: str) -> bool:
             e = traceback.format_exc()
             logger.error(f'Deleting row "{guid}" failed!\n{e}')
             status = trace.status.Status(trace.StatusCode.ERROR)
-            current_span.record_exception(e)
+            span.record_exception(e)
 
-        current_span.set_status(status)
+        span.set_status(status)
     return schemas.FakenamesDelete(**response)
