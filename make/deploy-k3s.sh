6df1cf2808fcdb652c20927908ad1f4831e8c6f1
Helm Student <student.helm@testruction.lab>
2023-09-18 15:45:04 -0400

    Fixed resources labels

--- make/deploy-k3s.sh
+++ make/deploy-k3s.sh
@@ -4,10 +4,12 @@ helm upgrade --install \
   --create-namespace \
   --namespace fastapi-demo \
   --set cockroachdb.statefulset.replicas="1" \
-  --set cockroachdb.service.public.type="NodePort" \
+  --set cockroachdb.service.public.type="LoadBalancer" \
   --set cockroachdb.storage.persistentVolume.size="1Gi" \
-  --set backend.service.type="NodePort" \
+  --set backend.service.type="LoadBalancer" \
   --set backend.serviceMonitor.enabled="true" \
-  --set frontend.service.type="NodePort" \
+  --set frontend.service.type="LoadBalancer" \
   --set frontend.serviceMonitor.enabled="true" \
   demo ./chart/
+
+kubectl label namespace fastapi-demo "prometheus=enabled"
